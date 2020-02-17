bee528d92bc41ad3ceeee6179987c1e62cbc12fa
taka-sun <takaho0729@gmail.com>
2020-02-17 22:38:08 +0900

    Add getAllMessages

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -3,14 +3,13 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Tasks;
+import models.Message;
 import utils.DBUtil;
 
 /**
@@ -34,15 +33,10 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Tasks> tasks = em.createNamedQuery("getAllTasks", Tasks.class)
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class)
                                    .getResultList();
+        response.getWriter().append(Integer.valueOf(messages.size()).toString());
 
         em.close();
-
-        request.setAttribute("tasks", tasks);
-
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
-        rd.forward(request, response);
     }
-
 }
